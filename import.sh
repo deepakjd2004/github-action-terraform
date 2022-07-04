823918bee5243159894be13289b5ab170e71aa23
Deepak Jha <deepakjd2004@gmail.com>
2022-07-04 14:22:59 +1000

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,3 +1,3 @@
 terraform init
-terraform import akamai_edge_hostname.www-this-site-does-not-exist-com-au-edgesuite-net ehn_5110236,ctr_C-1ED34DY,grp_178772
-terraform import akamai_property.Devops-Training prp_797614,ctr_C-1ED34DY,grp_178772
+terraform import akamai_edge_hostname.www-this-site-does-not-exist-com-au-edgesuite-net 
+terraform import akamai_property.Devops-Training
